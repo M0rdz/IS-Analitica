3b0f875e76e3e523463a98e91dc7cc256fa8ecbd
M0rdz <hectorovs12@gmail.com>
2022-07-27 17:08:37 -0500

    Loader

--- app.js
+++ app.js
@@ -83,3 +83,11 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+//Loader
+
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+  loader.style.display = "none";
+})
