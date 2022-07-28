595bcc06036d09de9b84ff7291ff479183c1acc3
M0rdz <hectorovs12@gmail.com>
2022-07-27 21:15:15 -0500

    mc

--- app.js
+++ app.js
@@ -63,12 +63,11 @@ function validate() {
   return( true );
 }
 
-// Top Button Function
+//Boton que regresa al inicio
 
-//Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// mostrarl el boton cuando se hace un scroll de 20px
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -79,13 +78,13 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// al hacer click regresar al incio
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }
 
-//Loader
+// Loader
 
 var loader = document.getElementById("preloader");
 
